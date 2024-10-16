fe9bd744217759a03e6d03b64636173f440426a9
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-10-16 20:27:11 -0300

    Config: Configura baseURL no tsconfig

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app'
-import { env } from './env/env'
+import { env } from '@/env/env'
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('🚀 Server is running! http://localhost:' + env.PORT)
