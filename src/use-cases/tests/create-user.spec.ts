88d6abaca35b3ed038b5328c0c093fd3085d116d
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-10-17 19:25:59 -0300

    teste: Finaliza primeira seção de testes na criação de usuário

--- src/use-cases/tests/create-user.spec.ts
+++ src/use-cases/tests/create-user.spec.ts
@@ -1,6 +1,8 @@
 import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-user-repository'
 import { describe, expect, test, beforeEach } from 'vitest'
 import { CreateUserUseCase } from '../create-user'
+import { EmailAlreadyExistsError } from '../Error/email-already-exists-error'
+import { compare } from 'bcryptjs'
 
 let userRepository: InMemoryUserRepository
 let sup: CreateUserUseCase
@@ -11,7 +13,7 @@ describe('Criação de usuário', () => {
     sup = new CreateUserUseCase(userRepository)
   })
 
-  test('Deve ser possível criar um usuário', async () => {
+  test('Deve ser possível cadastrar um usuário.', async () => {
     const { user } = await sup.execute({
       name: 'Jonh Doe',
       email: 'jonhdoe@example.com',
@@ -19,5 +21,33 @@ describe('Criação de usuário', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  test('Não deve ser possível cadastrar um usuário com um email ja existente.', async () => {
+    await sup.execute({
+      name: 'Jonh Doe',
+      email: 'jonhdoe@example.com',
+      password: '123456',
+    })
+
+    expect(() =>
+      sup.execute({
+        name: 'Jonh Doe',
+        email: 'jonhdoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(EmailAlreadyExistsError)
+  })
+
+  test('Deve ser possível encaminhar uma senha encriptografada para o banco de dados', async () => {
+    const { user } = await sup.execute({
+      name: 'Jonh Doe',
+      email: 'jonhdoe@example.com',
+      password: '123456',
+    })
+
+    const passhwordHashed = await compare('123456', user.password_hash)
+
+    expect(passhwordHashed).toEqual(true)
   })
 })
