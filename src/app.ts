e917d5e3bb698c2965fc7fd0167e1ac8cb64c9af
Rhuan Guilherme <rhuan23032004@gmail.com>
2024-10-19 19:45:56 -0300

    Criando handler de erros global

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
 import { userRoutes } from './http/controllers/users/routes'
+import { ZodError } from 'zod'
+import { env } from './env/env'
 
 export const app = fastify()
 
 app.register(userRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Erro de validação', description: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal Server Error.' })
+})
